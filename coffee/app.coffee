2349626effe4c39ed0a75ee5eb7d137148fb8948
Nyura95 <taxisoopya@hotmail.fr>
2015-11-04 11:25:14 +0100

    Add change master and life

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -4,70 +4,124 @@ ws             = require "nodejs-websocket"
 players = new Array()
 
 class Player
+  constructor: (@socket, @id) ->
+    @life = 100
+    @master = false
+    try
+      @socket.sendText JSON.stringify ({
+        opcode: 1
+      })
+      @close @socket
+      @text @socket, @id
+      @ennemi
+    catch err
+      console.log "constructor"
+      console.log err
   close: (@socket) ->
-    @socket.on 'close', (code, reason) =>
-      for player in players
-        if player.id isnt @id
+    try
+      @socket.on 'close', (code, reason) =>
+        master = false
+        for player in players
+          if player.id isnt @id
+            player.socket.sendText JSON.stringify ({
+              opcode: 5
+              id: @id
+            })
+          if player.id is @id
+            master = true
+        players.splice players.indexOf(@), 1
+        console.log 'Connection closed'
+        if master
+          players[0].master = true
           player.socket.sendText JSON.stringify ({
-            opcode: 5
-            id: @id
+            opcode: 0
+            id: players[0].id
+            master:players[0].master
           })
-      players.splice players.indexOf(@), 1
-      console.log 'Connection closed'
+        console.log "Master changed (#{players[0].id})"
+    catch err
+      console.log "close"
+      console.log err
+
   text: (@socket, @id) ->
     @socket.on 'text', (str) =>
-      json = JSON.parse str
+      try
+        json = JSON.parse str
+      catch err
+        console.log "json : #{err}"
       switch json.opcode
+        when 10
+          for player in players
+            if player.id is json.id
+              #console.log "players :#{player.id} touché !"
+              player.life = player.life - 20
+              if player.life < 0
+                player.life = 0
+              player.socket.sendText JSON.stringify ({
+                opcode: 10
+                id:player.id
+                life:player.life
+              })
         when 6
           #missile
-          for player in players
-            if player.id isnt @id
-              player.socket.sendText JSON.stringify ({
-                opcode: 7
-                id: @id
-                type:json.type
-                x: json.position.x
-                y: json.position.y
-              })
+          try
+            for player in players
+              if player.id isnt @id
+                player.socket.sendText JSON.stringify ({
+                  opcode: 7
+                  id: @id
+                  type:json.type
+                  x: json.position.x
+                  y: json.position.y
+                })
+          catch err
+            console.log "missile"
+            console.log err
 
         when 2
           #connection player
-          @id = json.id
-          for player in players
-            if player.id isnt @id
+          try
+            @id = json.id
+            for player in players
+              if player.id isnt @id
+                player.socket.sendText JSON.stringify ({
+                  opcode: 3
+                  id: @id
+                })
+
+            if players.length is 1
+              player.master = true
               player.socket.sendText JSON.stringify ({
-                opcode: 3
+                opcode: 0
                 id: @id
+                master:true
               })
 
-          for player in players
-            if player.id isnt @id
-              @socket.sendText JSON.stringify ({
-                opcode: 3
-                id: player.id
-              })
+            for player in players
+              if player.id isnt @id
+                @socket.sendText JSON.stringify ({
+                  opcode: 3
+                  id: player.id
+                })
 
-          console.log "Player #{@id} connected !"
+            console.log "Player #{@id} connected !"
+          catch err
+            console.log "connection player"
+            console.log err
         when 4
           #position
-          for player in players
-            if player.id isnt @id
-              player.socket.sendText JSON.stringify ({
-                opcode: 4
-                id: @id
-                x: json.position.x
-                y: json.position.y
-              })
-
-  constructor: (@socket, @id) ->
-    @socket.sendText JSON.stringify ({
-      opcode: 1
-    })
-    @close @socket
-    @text @socket, @id
-    @ennemi
-
-
+          try
+            for player in players
+              if player.id isnt @id
+                player.socket.sendText JSON.stringify ({
+                  opcode: 4
+                  id: @id
+                  x: json.position.x
+                  y: json.position.y
+                })
+          catch err
+            console.log "position"
+            console.log err
 
 class Application
   constructor : ->
@@ -85,6 +139,9 @@ class Application
 
     ennemi = new Ennemi @connection
 
+    process.on 'uncaughtException', (err) ->
+      console.error err.stack
+
   setupDatabase: (base) ->
     @connection = mysql.createConnection {
       socketPath : '/Applications/MAMP/tmp/mysql/mysql.sock',
@@ -97,20 +154,31 @@ class Application
 class Ennemi
   constructor: (@connection) ->
     setInterval =>
-      id = Math.floor((Math.random() * 2) + 1)
-      x = Math.floor((Math.random() * 1080) + 1)
-      y = Math.floor((Math.random() * 1080) + 1)
-      @generate id, (rows) =>
-        for player in players
-          player.socket.sendText JSON.stringify ({
-            opcode:9
-            ennemi:rows
-            x: x
-            y: y
-          })
+      try
+        random = Math.floor((Math.random() * 10) + 1)
+        if random is 10
+          id = 3
+        else
+          id = Math.floor((Math.random() * 2) + 1)
+        x = Math.floor((Math.random() * 1080) + 1)
+        y = Math.floor((Math.random() * 1080) + 1)
+        @generate id, (rows) =>
+          for player in players
+            player.socket.sendText JSON.stringify ({
+              opcode:9
+              ennemi:rows
+              x: x
+              y: y
+            })
+      catch err
+        console.log "ennemi"
+        console.log err
     , 200
   generate: (id, callback) ->
     @connection.query 'select * from ennemie, arme where ennemie.id_arme=arme.id_arme AND ennemie.id_ennemie = ?', [id], (err, rows) =>
+      if err
+        console.log "request"
+        console.log err
       callback rows[0]
 
 
--- js/app.js
+++ js/app.js
@@ -8,24 +8,64 @@
   players = new Array();
 
   Player = (function() {
-    Player.prototype.close = function(socket) {
+    function Player(socket, id) {
+      var err;
       this.socket = socket;
-      return this.socket.on('close', (function(_this) {
-        return function(code, reason) {
-          var player, _i, _len;
-          for (_i = 0, _len = players.length; _i < _len; _i++) {
-            player = players[_i];
-            if (player.id !== _this.id) {
+      this.id = id;
+      this.life = 100;
+      this.master = false;
+      try {
+        this.socket.sendText(JSON.stringify({
+          opcode: 1
+        }));
+        this.close(this.socket);
+        this.text(this.socket, this.id);
+        this.ennemi;
+      } catch (_error) {
+        err = _error;
+        console.log("constructor");
+        console.log(err);
+      }
+    }
+
+    Player.prototype.close = function(socket) {
+      var err;
+      this.socket = socket;
+      try {
+        return this.socket.on('close', (function(_this) {
+          return function(code, reason) {
+            var master, player, _i, _len;
+            master = false;
+            for (_i = 0, _len = players.length; _i < _len; _i++) {
+              player = players[_i];
+              if (player.id !== _this.id) {
+                player.socket.sendText(JSON.stringify({
+                  opcode: 5,
+                  id: _this.id
+                }));
+              }
+              if (player.id === _this.id) {
+                master = true;
+              }
+            }
+            players.splice(players.indexOf(_this), 1);
+            console.log('Connection closed');
+            if (master) {
+              players[0].master = true;
               player.socket.sendText(JSON.stringify({
-                opcode: 5,
-                id: _this.id
+                opcode: 0,
+                id: players[0].id,
+                master: players[0].master
               }));
             }
-          }
-          players.splice(players.indexOf(_this), 1);
-          return console.log('Connection closed');
-        };
-      })(this));
+            return console.log("Master changed (" + players[0].id + ")");
+          };
+        })(this));
+      } catch (_error) {
+        err = _error;
+        console.log("close");
+        return console.log(err);
+      }
     };
 
     Player.prototype.text = function(socket, id) {
@@ -33,20 +73,27 @@
       this.id = id;
       return this.socket.on('text', (function(_this) {
         return function(str) {
-          var json, player, _i, _j, _k, _l, _len, _len1, _len2, _len3, _results, _results1;
-          json = JSON.parse(str);
+          var err, json, player, _i, _j, _k, _l, _len, _len1, _len2, _len3, _len4, _m, _results, _results1, _results2;
+          try {
+            json = JSON.parse(str);
+          } catch (_error) {
+            err = _error;
+            console.log("json : " + err);
+          }
           switch (json.opcode) {
-            case 6:
+            case 10:
               _results = [];
               for (_i = 0, _len = players.length; _i < _len; _i++) {
                 player = players[_i];
-                if (player.id !== _this.id) {
+                if (player.id === json.id) {
+                  player.life = player.life - 20;
+                  if (player.life < 0) {
+                    player.life = 0;
+                  }
                   _results.push(player.socket.sendText(JSON.stringify({
-                    opcode: 7,
-                    id: _this.id,
-                    type: json.type,
-                    x: json.position.x,
-                    y: json.position.y
+                    opcode: 10,
+                    id: player.id,
+                    life: player.life
                   })));
                 } else {
                   _results.push(void 0);
@@ -54,58 +101,92 @@
               }
               return _results;
               break;
+            case 6:
+              try {
+                _results1 = [];
+                for (_j = 0, _len1 = players.length; _j < _len1; _j++) {
+                  player = players[_j];
+                  if (player.id !== _this.id) {
+                    _results1.push(player.socket.sendText(JSON.stringify({
+                      opcode: 7,
+                      id: _this.id,
+                      type: json.type,
+                      x: json.position.x,
+                      y: json.position.y
+                    })));
+                  } else {
+                    _results1.push(void 0);
+                  }
+                }
+                return _results1;
+              } catch (_error) {
+                err = _error;
+                console.log("missile");
+                return console.log(err);
+              }
+              break;
             case 2:
-              _this.id = json.id;
-              for (_j = 0, _len1 = players.length; _j < _len1; _j++) {
-                player = players[_j];
-                if (player.id !== _this.id) {
+              try {
+                _this.id = json.id;
+                for (_k = 0, _len2 = players.length; _k < _len2; _k++) {
+                  player = players[_k];
+                  if (player.id !== _this.id) {
+                    player.socket.sendText(JSON.stringify({
+                      opcode: 3,
+                      id: _this.id
+                    }));
+                  }
+                }
+                if (players.length === 1) {
+                  player.master = true;
                   player.socket.sendText(JSON.stringify({
-                    opcode: 3,
-                    id: _this.id
-                  }));
-                }
-              }
-              for (_k = 0, _len2 = players.length; _k < _len2; _k++) {
-                player = players[_k];
-                if (player.id !== _this.id) {
-                  _this.socket.sendText(JSON.stringify({
-                    opcode: 3,
-                    id: player.id
-                  }));
-                }
-              }
-              return console.log("Player " + _this.id + " connected !");
-            case 4:
-              _results1 = [];
-              for (_l = 0, _len3 = players.length; _l < _len3; _l++) {
-                player = players[_l];
-                if (player.id !== _this.id) {
-                  _results1.push(player.socket.sendText(JSON.stringify({
-                    opcode: 4,
+                    opcode: 0,
                     id: _this.id,
-                    x: json.position.x,
-                    y: json.position.y
-                  })));
-                } else {
-                  _results1.push(void 0);
+                    master: true
+                  }));
                 }
+                for (_l = 0, _len3 = players.length; _l < _len3; _l++) {
+                  player = players[_l];
+                  if (player.id !== _this.id) {
+                    _this.socket.sendText(JSON.stringify({
+                      opcode: 3,
+                      id: player.id
+                    }));
+                  }
+                }
+                return console.log("Player " + _this.id + " connected !");
+              } catch (_error) {
+                err = _error;
+                console.log("connection player");
+                return console.log(err);
               }
-              return _results1;
+              break;
+            case 4:
+              try {
+                _results2 = [];
+                for (_m = 0, _len4 = players.length; _m < _len4; _m++) {
+                  player = players[_m];
+                  if (player.id !== _this.id) {
+                    _results2.push(player.socket.sendText(JSON.stringify({
+                      opcode: 4,
+                      id: _this.id,
+                      x: json.position.x,
+                      y: json.position.y
+                    })));
+                  } else {
+                    _results2.push(void 0);
+                  }
+                }
+                return _results2;
+              } catch (_error) {
+                err = _error;
+                console.log("position");
+                return console.log(err);
+              }
           }
         };
       })(this));
     };
-
-    function Player(socket, id) {
-      this.socket = socket;
-      this.id = id;
-      this.socket.sendText(JSON.stringify({
-        opcode: 1
-      }));
-      this.close(this.socket);
-      this.text(this.socket, this.id);
-      this.ennemi;
-    }
 
     return Player;
 
@@ -124,7 +205,10 @@
         console.log('New connection');
         return players.push(new Player(conn, -1));
       }).listen(3000);
-      return ennemi = new Ennemi(this.connection);
+      ennemi = new Ennemi(this.connection);
+      return process.on('uncaughtException', function(err) {
+        return console.error(err.stack);
+      });
     };
 
     Application.prototype.setupDatabase = function(base) {
@@ -146,24 +230,35 @@
       this.connection = connection;
       setInterval((function(_this) {
         return function() {
-          var id, x, y;
-          id = Math.floor((Math.random() * 2) + 1);
-          x = Math.floor((Math.random() * 1080) + 1);
-          y = Math.floor((Math.random() * 1080) + 1);
-          return _this.generate(id, function(rows) {
-            var player, _i, _len, _results;
-            _results = [];
-            for (_i = 0, _len = players.length; _i < _len; _i++) {
-              player = players[_i];
-              _results.push(player.socket.sendText(JSON.stringify({
-                opcode: 9,
-                ennemi: rows,
-                x: x,
-                y: y
-              })));
+          var err, id, random, x, y;
+          try {
+            random = Math.floor((Math.random() * 10) + 1);
+            if (random === 10) {
+              id = 3;
+            } else {
+              id = Math.floor((Math.random() * 2) + 1);
             }
-            return _results;
-          });
+            x = Math.floor((Math.random() * 1080) + 1);
+            y = Math.floor((Math.random() * 1080) + 1);
+            return _this.generate(id, function(rows) {
+              var player, _i, _len, _results;
+              _results = [];
+              for (_i = 0, _len = players.length; _i < _len; _i++) {
+                player = players[_i];
+                _results.push(player.socket.sendText(JSON.stringify({
+                  opcode: 9,
+                  ennemi: rows,
+                  x: x,
+                  y: y
+                })));
+              }
+              return _results;
+            });
+          } catch (_error) {
+            err = _error;
+            console.log("ennemi");
+            return console.log(err);
+          }
         };
       })(this), 200);
     }
@@ -171,6 +266,10 @@
     Ennemi.prototype.generate = function(id, callback) {
       return this.connection.query('select * from ennemie, arme where ennemie.id_arme=arme.id_arme AND ennemie.id_ennemie = ?', [id], (function(_this) {
         return function(err, rows) {
+          if (err) {
+            console.log("request");
+            console.log(err);
+          }
           return callback(rows[0]);
         };
       })(this));
